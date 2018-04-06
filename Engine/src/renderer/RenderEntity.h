8d39d6081cc5d5511ae2481d46de530d1d394807
Owlinated <florian@stellbr.ink>
2018-04-07 00:43:05 +0200

    Smooth shade all the things

--- Engine/src/renderer/RenderEntity.h
+++ Engine/src/renderer/RenderEntity.h
@@ -7,7 +7,7 @@
 #include <src/renderer/geometry/Shape.h>
 
 struct Material {
-  Material() : ambient_multiplier(0.1, 0.1, 0.1), diffuse_multiplier(1, 1, 1), specular_multiplier(0.3, 0.3, 0.3) {}
+  Material() : ambient_multiplier(0.2, 0.2, 0.2), diffuse_multiplier(1, 1, 1), specular_multiplier(1, 1, 1) {}
   glm::vec3 ambient_multiplier;
   glm::vec3 diffuse_multiplier;
   glm::vec3 specular_multiplier;
