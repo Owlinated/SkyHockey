8c269afb64a9ef42d6a55a3efd1a8b95a50a1c86
Unknown <florian@stellbr.ink>
2018-04-21 01:52:53 +0200

    Cache shader uniform locations

--- Engine/src/renderer/material/Shader.cpp
+++ Engine/src/renderer/material/Shader.cpp
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <GL/glew.h>
 #include <src/support/Formatter.h>
+#include <map>
 #include "Shader.h"
 
 void checkShaderError(GLuint handle, std::string file_path) {
@@ -37,7 +38,7 @@ void checkProgramError(GLuint handle, std::string vertex_path, std::string fragm
   }
 }
 
-Shader::Shader(const char *vertex_file_path, const char *fragment_file_path) {
+Shader::Shader(const std::string &vertex_file_path, const std::string &fragment_file_path) {
   std::ifstream vertex_stream(vertex_file_path);
   std::stringstream vertex_buffer;
   vertex_buffer << vertex_stream.rdbuf();
@@ -77,42 +78,54 @@ void Shader::use() {
   glUseProgram(handle);
 }
 
-GLint Shader::getUniform(const char *uniform_name) {
-  // TODO cache locations
-  auto result = glGetUniformLocation(handle, uniform_name);
+GLint Shader::getUniform(const std::string &uniform_name) {
+  // Look name up in cache
+  auto cache_result = cache_.find(uniform_name);
+  if (cache_result != cache_.end()) {
+    return cache_result->second;
+  }
+
+  // Look name up in shader
+  auto result = glGetUniformLocation(handle, uniform_name.data());
   if (result < 0) {
     throw std::runtime_error(Formatter() << "Could not find uniform: " << std::string(uniform_name));
   }
+
+  // Create cache entry and return value
+  cache_[uniform_name] = result;
   return result;
 }
 
-void Shader::bind(std::shared_ptr<Texture> &texture, const char *uniform_name, int unit_index) {
+void Shader::bind(std::shared_ptr<Texture> &texture, const std::string &uniform_name, int unit_index) {
   // Activate texture unit and bind the texture
   glActiveTexture(static_cast<GLenum>(GL_TEXTURE0 + unit_index));
   texture->bind();
   bind(unit_index, uniform_name);
 }
 
-void Shader::bind(int scalar, const char *uniform_name) {
+void Shader::bind(int scalar, const std::string &uniform_name) {
   glUniform1i(getUniform(uniform_name), scalar);
 }
 
-void Shader::bind(float scalar, const char *uniform_name) {
+void Shader::bind(float scalar, const std::string &uniform_name) {
   glUniform1f(getUniform(uniform_name), scalar);
 }
 
-void Shader::bind(glm::vec2 &vector, const char *uniform_name) {
+void Shader::bind(glm::vec2 &vector, const std::string &uniform_name) {
   glUniform2f(getUniform(uniform_name), vector.x, vector.y);
 }
 
-void Shader::bind(glm::vec3 &vector, const char *uniform_name) {
+void Shader::bind(glm::vec3 &vector, const std::string &uniform_name) {
   glUniform3f(getUniform(uniform_name), vector.x, vector.y, vector.z);
 }
 
-void Shader::bind(glm::vec4 &vector, const char *uniform_name) {
+void Shader::bind(glm::vec4 &vector, const std::string &uniform_name) {
   glUniform4f(getUniform(uniform_name), vector.x, vector.y, vector.z, vector.w);
 }
 
-void Shader::bind(glm::mat4 &matrix, const char *uniform_name) {
+void Shader::bind(glm::mat4 &matrix, const std::string &uniform_name) {
   glUniformMatrix4fv(getUniform(uniform_name), 1, GL_FALSE, &matrix[0][0]);
+}
+void Shader::bind(std::vector<int> &vector, const std::string &uniform_name) {
+  glUniform1iv(getUniform(uniform_name), vector.size(), vector.data());
 }
--- Engine/src/renderer/material/Shader.h
+++ Engine/src/renderer/material/Shader.h
@@ -3,21 +3,25 @@
 
 #include <glm/vec2.hpp>
 #include <glm/glm.hpp>
+#include <map>
 #include "Texture.h"
 
 class Shader {
+ private:
+  std::map<std::string, int> cache_;
  public:
-  Shader(const char * vertex_file_path, const char * fragment_file_path);
+  Shader(const std::string & vertex_file_path, const std::string & fragment_file_path);
   GLuint handle;
   void use();
-  GLint getUniform(const char *uniform_name);
-  void bind(std::shared_ptr<Texture>& texture, const char* uniform_name, int unit_index);
-  void bind(int scalar, const char* uniform_name);
-  void bind(float scalar, const char* uniform_name);
-  void bind(glm::vec2 &vector, const char* uniform_name);
-  void bind(glm::vec3 &vector, const char* uniform_name);
-  void bind(glm::vec4 &vector, const char* uniform_name);
-  void bind(glm::mat4 &matrix, const char* uniform_name);
+  GLint getUniform(const std::string &uniform_name);
+  void bind(std::shared_ptr<Texture> &texture, const std::string &uniform_name, int unit_index);
+  void bind(int scalar, const std::string &uniform_name);
+  void bind(float scalar, const std::string &uniform_name);
+  void bind(glm::vec2 &vector, const std::string &uniform_name);
+  void bind(glm::vec3 &vector, const std::string &uniform_name);
+  void bind(glm::vec4 &vector, const std::string &uniform_name);
+  void bind(glm::mat4 &matrix, const std::string &uniform_name);
+  void bind(std::vector<int> &vector, const std::string &uniform_name);
 };
 
 #endif //SHADER_HPP
