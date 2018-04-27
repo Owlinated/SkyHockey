dafee3883a362a3f0ffdd24e71a0aa86eb3c2d8b
Unknown <florian@stellbr.ink>
2018-04-27 21:21:11 +0200

    Use higher quality rendering by default

--- Engine/src/Config.cpp
+++ Engine/src/Config.cpp
@@ -4,7 +4,7 @@ bool Config::full_screen;
 
 bool Config::forward_rendering;
 
-bool Config::fancy_background;
+bool Config::fancy_background = true;
 
 int Config::shadow_blur_size = 4;
 
@@ -12,11 +12,11 @@ int Config::motion_blur_steps = 8;
 
 float Config::motion_blur_step_size = 0.5f;
 
-bool Config::perf_overlay = false;
+bool Config::perf_overlay;
 
 float Config::perf_overlay_scale = 5.0f;
 
-int Config::anti_aliasing_level = 2;
+int Config::anti_aliasing_level = 3;
 
 void Config::parse(std::vector<std::string> arguments) {
   for (const auto &arg: arguments) {
