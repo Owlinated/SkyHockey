b774a21060445840f7e3498204ce52097ade8b5b
Florian Stellbrink <flo@stellbr.ink>
2019-09-25 19:30:46 +0200

    Replace glm and libpng with conan-center dependencies

--- Engine/conanfile.py
+++ Engine/conanfile.py
@@ -1,10 +1,10 @@
 from conans import ConanFile, CMake
 
 class Engine(ConanFile):
-    requires = ("glm/0.9.9.1@g-truc/stable",
+    requires = ("glm/0.9.9.5@_/_",
                 "glad/0.1.29@bincrafters/stable",
                 "glfw/3.3@bincrafters/stable",
-                "libpng/1.6.36@bincrafters/stable",
+                "libpng/1.6.37@_/_",
                 "tinyobjloader/1.0.6@_/_"
                 )
     settings = "os", "compiler", "build_type", "arch"
